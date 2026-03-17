e8e6663d1564f1f6596f7c4130434de3b33753d7
ja0nz <git@ja.nz>
2026-03-17 16:08:53 +0100

    feat(beszel): added agent directory

--- shared-modules/beszelagent.nix
+++ shared-modules/beszelagent.nix
@@ -2,7 +2,7 @@
   Module: beszelagent.nix
   Description: Lightweight server monitoring hub with historical data, docker stats, and alerts
   * Detailed info:
-  - Persist & Backup: /var/lib/beszel-agent
+  - Persist & Backup: /var/lib/private (done in impermanence anyway)
 */
 {
   config,
@@ -10,28 +10,22 @@
   ...
 }:
 
-# let
-#   dataDir = "/var/lib/beszel-agent";
-# in
+let
+  dataDir = "/var/lib/beszel-agent";
+in
 {
-  # sysOpts.persist.directories = [
-  #   {
-  #     directory = dataDir;
-  #     user = "beszel-agent";
-  #   }
-  # ];
-
   sops.secrets."beszel_token" = { };
   sops.secrets."beszel_sshKey" = { };
   sops.templates."beszel.env" = {
     content = ''
-      # DATA_DIR=dataDir
+      DATA_DIR=${dataDir}
       KEY=${config.sops.placeholder."beszel_sshKey"}
       TOKEN=${config.sops.placeholder."beszel_token"}
       HUB_URL=https://mon.${vars.DOMAIN}
     '';
   };
 
+  systemd.services.beszel-agent.serviceConfig.StateDirectory = "beszel-agent";
   services.beszel.agent = {
     enable = true;
     openFirewall = false; # agent initiates outbound WebSocket to hub, so not needed
